7a3902b4964ecd7fbf20e35da197f1d0f4e63cc3
NicoBarti <nicolas.barticevic@gmail.com>
2019-07-03 02:16:37 -0400

    cambio a pg database para heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_07_02_213111) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.decimal "itemid"
     t.decimal "alt"
